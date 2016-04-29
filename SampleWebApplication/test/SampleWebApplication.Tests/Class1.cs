2d1909f2fac62920dd3487699472584972675e94
Mark Michaelis - Personal <Mark Michaelis - Personal>
2016-04-28 17:11:46 -0700

    Added a unit test

--- SampleWebApplication/test/SampleWebApplication.Tests/Class1.cs
+++ SampleWebApplication/test/SampleWebApplication.Tests/Class1.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace SampleWebApplication.Tests
 {
     // This project can output the Class library as a NuGet Package.
     // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
-    public class Class1
+    public class Tests
     {
-        public Class1()
+        [Fact]
+        public void ReallyGreatUnitTest()
         {
+            Assert.True( 42==42, "Pheww!!");
         }
     }
 }
